835d395d3a1d099a5555633ee18beae6afa35b47
baladewa <tkjmaikel@gmail.com>
2023-01-05 22:34:00 +0700

    first commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Dashboard extends CI_Controller{
+    
+    public function __construct(){
+        parent::__construct();
+
+        if($this->session->userdata('status') != "login"){
+            redirect(base_url("login")); 
+        }
+    }
+
+    public function index(){
+        $this->load->view('/page/index');
+    }
+}
+
+?>
